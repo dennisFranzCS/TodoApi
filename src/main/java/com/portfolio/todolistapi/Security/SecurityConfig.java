bdf214caeefaec7648e4e494b257a9284050bdaa
dennisFranzCS <dennisfranz@protonmail.com>
2023-01-22 21:03:04 +0100

    Konfiguration von SpringSecurity für Resource Server erstellt.
    Konfiguration für SpringSecurity für:
    - Authentisierung an allen Endpunkten
    - Stateless SessionManagement
    - OAuth2 Resource Server basierte Authentisierung durch JWT

---
+++ src/main/java/com/portfolio/todolistapi/Security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.portfolio.todolistapi.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .headers(headers -> headers.frameOptions().sameOrigin())
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return http.build();
+    }
+
+}
